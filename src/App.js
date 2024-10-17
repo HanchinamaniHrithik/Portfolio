936a2e5e083af90fee5b291d72be6ed07ae54c36
HanchinamaniHrithik <hrithikhanchinamani@gmail.com>
2024-10-17 18:33:13 +0530

    Skeleton of portfolio created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h2>
+        <center>
+          Hi. This is the kickstart to my personal website/portfolio, please
+          wait till I finish, thanks :)
+        </center>
+      </h2>
+    </div>
+  )
+}
+
+export default App
